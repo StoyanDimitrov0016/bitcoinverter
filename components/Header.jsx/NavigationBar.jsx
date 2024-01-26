5f0a82fa3f999d8d65dab01da2b37fb1f04555f2
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-01-26 13:30:01 +0200

    feat(navigation): create NavigationBar component

---
+++ components/Header.jsx/NavigationBar.jsx
@@ -0,0 +1,17 @@
+const NavigationBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Convert</li>
+      </ul>
+      <ul>
+        <li>Learn</li>
+      </ul>
+      <ul>
+        <li>Price</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
