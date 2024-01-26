19a8a1b988ee826032336591b2d2e3c356f6ec13
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-01-26 13:30:44 +0200

    feat(header): create Header component with BitCoinverter title and NavigationBar integration

---
+++ components/Header.jsx/Header.jsx
@@ -0,0 +1,12 @@
+import NavigationBar from "./NavigationBar";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>BitCoinverter</h1>
+      <NavigationBar />
+    </header>
+  );
+};
+
+export default Header;
